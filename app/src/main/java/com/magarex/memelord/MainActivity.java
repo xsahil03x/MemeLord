31a9d8d4eb66d61112c7fe3c11bcb9dedde8f5c7
Sahil Kumar <xdsahil@gmail.com>
2018-10-23 18:06:49 +0530

    Migrate to Android-X

--- app/src/main/java/com/magarex/memelord/MainActivity.java
+++ app/src/main/java/com/magarex/memelord/MainActivity.java
@@ -1,6 +1,6 @@
 package com.magarex.memelord;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
